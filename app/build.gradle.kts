c77dd7e76d1e1b22b8bb25cf8cbf20c7c061282c
avill2022 <avill.vg@gmail.com>
2026-08-19 13:54:59 -0600

    versionCode = 5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "avill.ladv.chordo"
         minSdk = 25
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.3"
+        versionCode = 5
+        versionName = "1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
